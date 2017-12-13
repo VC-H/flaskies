be37c0d57db3ac70525d2410883dd5c1c4ff2478
Victor Hui <23016403+vc-h@users.noreply.github.com>
2017-12-13 20:14:04 +0000

    new modules: templateview.py, basics.py:
    	new file:   templateview.py
    	new file:   basics.py
    
    * new templates for viewing the html sources produced by the templates:
    
    	new file:   templates/templateview.htm
    	new file:   static/css/source.css

---
+++ basics.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+flaskies.basics
+===============
+
+:class:`Flask` app rules table and call stack introspections
+------------------------------------------------------------
+
+.. ::
+  Copyright: (c) 2017 by Victor Hui.
+  Licence: BSD-3-Clause (see LICENSE for more details)
+
+* create a :py:obj:`basics` :class:`Blueprint`
+
+.. code-block:: python
+
+   basics = Blueprint('basics',import_name=__name__)
+
+* Basics of :class:`Flask` application
+
+  - :meth:`rulestable` shows a table of rule map and
+    the templates used
+  - :meth:`stacktables` shows a table of libraries and
+    modules of the call stack
+  - useful to work with :py:obj:`templateview` :class:`Blueprint`
+
+* re-used:
+
+  - :meth:`hello.href`
+  - :meth:`templateview.view`
+  - :meth:`templateview.listall`
+
+* define two :py:obj:`view_func`'s:
+
+.. literalinclude:: ../basics.py
+   :pyobject: rulestable
+
+.. literalinclude:: ../basics.py
+   :pyobject: stacktable
+
+-----
+"""
+
+from __future__ import print_function, unicode_literals
+import sys, os, re, traceback, inspect
+from flask import (
+    Flask, Blueprint, render_template, current_app, url_for,
+    escape, Markup)
+from jinja2 import meta
+from hello import href
+
+re_sub_rule_variable = re.compile(
+    """<[^/]+>[/]*""").sub
+re_findall_templates = re.compile(
+    """[(\s]render_template\s*\(\s*['"](.+?)['"]""",
+    flags=re.DOTALL).findall # apply to inspect.getsource(viewfunction)
+
+def getendtemplates():
+    """return a :py:obj:`dict` of :data:`[(endpoint,templates),...]`;
+
+    >>> testapp = Flask(__name__)
+    >>> with testapp.app_context():
+    ...     templatesdict = getendtemplates()
+    >>> sorted(templatesdict.items()) == [
+    ...     ('static',[])]
+    True
+    >>> testapp.register_blueprint(basics)
+    >>> with testapp.app_context():
+    ...     templatesdict = getendtemplates()
+    >>> sorted(templatesdict.items()) == [
+    ...     ('basics.rulestable',['tableview.htm']),
+    ...     ('basics.stacktable',['tableview.htm']),
+    ...     ('static',[])]
+    True
+
+    """
+    return dict(
+        (endpoint,re_findall_templates(inspect.getsource(viewfunction)))
+        for endpoint,viewfunction in current_app.view_functions.items())
+
+def getrules():
+    """return a :py:obj:`list` of :data:`(methods,rule,endpoint,template)`;
+
+    >>> testapp = Flask(__name__)
+    >>> with testapp.app_context():
+    ...     table = getrules()
+    ...
+    >>> sorted(table.keys()) == ['headings','records']
+    True
+    >>> len(table.get('records'))
+    1
+    >>> table.get('records')[0] == (
+    ...     'GET, HEAD, OPTIONS',
+    ...     href('/static/','/static/<path:filename>'),
+    ...     'static',
+    ...     '')
+    True
+
+    """
+    templatesdict = getendtemplates()
+    rules = []
+    for rule in current_app.url_map.iter_rules():
+        methods = ", ".join(sorted(rule.methods))
+        url = re_sub_rule_variable("",rule.rule)
+        href_rule = href(url,descr=rule.rule)
+        if 'templateview' not in current_app.blueprints:
+            href_templates = Markup(', ').join(
+                templatesdict[rule.endpoint])
+        else:
+            href_templates = Markup(', ').join(
+                href(url_for('templateview.view',template=t),descr=t)
+                for t in templatesdict[rule.endpoint])
+        rules.append((methods,href_rule,rule.endpoint,href_templates))
+    return dict(
+        records = rules,
+        headings = ('methods','url','endpoint','templates'),)
+
+def tracereftemplates(jinja_env,template):
+    """return a :py:obj:`list` of all references in `template` recursively;
+
+    >>> testapp = Flask(__name__)
+    >>> with testapp.app_context():
+    ...     (tracereftemplates(testapp.jinja_env,'base.htm') ==
+    ...      tracereftemplates(current_app.jinja_env,'base.htm') ==
+    ...      [])
+    ...
+    True
+    >>> with testapp.app_context():
+    ...     (tracereftemplates(testapp.jinja_env,'tableview.htm') ==
+    ...      tracereftemplates(current_app.jinja_env,'tableview.htm') ==
+    ...      ['base.htm'])
+    ...
+    True
+
+    """
+    sourcetuple = jinja_env.loader.get_source(jinja_env,template)
+    ast = jinja_env.parse(sourcetuple)
+    templates = list(meta.find_referenced_templates(ast))
+    for template in set(templates):
+        templates.extend(tracereftemplates(jinja_env,template))
+    return sorted(list(set(templates)))
+
+def getalltemplates():
+    """return a :py:obj:`list` of templates used in :data:`flask.current_app`;
+
+    >>> testapp = Flask(__name__)
+    >>> testapp.register_blueprint(basics)
+    >>> with testapp.app_context():
+    ...     templates = getalltemplates()
+    >>> templates == [
+    ...     'base.htm',
+    ...     'tableview.htm',
+    ... ]
+    True
+
+    """
+    templatesdict = getendtemplates()
+    templates = []
+    list(map(templates.extend,templatesdict.values()))
+    jinja_env = current_app.jinja_env
+    for template in set(templates):
+        templates.extend(tracereftemplates(jinja_env,template))
+    return sorted(list(set(templates)))
+
+def getstack(skip=0,ignorelibdir=False):
+    """return the traceback from the caller;
+
+    * the stack table depends on the context of caller.
+    * the top stack is :func:`getstack` itself.
+
+    >>> table = getstack()
+    >>> sorted(table.keys()) == ['headings','records']
+    True
+    >>> len(table['records']) > 1
+    True
+    >>> caller = table['records'][0]
+    >>> caller[-1] == Markup(
+    ...     'for path,lineno,funcname,source '
+    ...     'in traceback.extract_stack():')
+    True
+    >>> caller[:2] == ('$APP/flaskies.github/basics.py','getstack')
+    True
+
+    """
+    appdir = os.path.dirname(os.getcwd())
+    libdir = os.path.dirname(os.__file__)
+    pkgdir = os.path.join(libdir,'site-packages')
+    stack = []
+    for path,lineno,funcname,source in traceback.extract_stack():
+        if path.startswith(appdir):
+            path = '$APP' + path[len(appdir):]
+        elif path.startswith(pkgdir):
+            path = '$SITE' + path[len(pkgdir):]
+        elif path.startswith(libdir):
+            if ignorelibdir:
+                continue
+            path = '$LIB' + path[len(libdir):]
+        stack.append((path,funcname,lineno,escape(source)))
+    stack.reverse()
+    return dict(
+        records = stack[skip:],
+        headings = ('file','function','lineno','source'),)
+
+
+basics = Blueprint('basics',__name__)
+
+@basics.route('/rulestable')
+def rulestable():
+    return render_template(
+        'tableview.htm', caption = 'url_map',
+        tables = [getrules(),])
+
+@basics.route('/stacktable')
+def stacktable():
+    return render_template(
+        'tableview.htm', caption = 'stack',
+        tables = [getstack(skip=0),])
+
+
+if __name__ == '__main__':
+
+    import sys
+    import doctest
+    from templateview import templateview
+
+    app = Flask(__name__)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.register_blueprint(templateview)
+    app.register_blueprint(basics)
+    app.add_url_rule("/",endpoint='basics.rulestable')
+
+    if sys.argv[0] != "":
+        app.run(debug=True,use_reloader=True)
+    else:
+        print(doctest.testmod(optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+        exec(doctest.script_from_examples(os.linesep.join([
+            getrules.__doc__,getstack.__doc__])))
---
+++ templateview.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+flaskies.templateview
+=====================
+
+Test the templates used in flaskies
+-----------------------------------
+
+.. ::
+  Copyright: (c) 2017 by Victor Hui.
+  Licence: BSD-3-Clause (see LICENSE for more details)
+
+* create a :py:obj:`templateview` :class:`Blueprint`
+
+  - useful to work with :py:obj:`basics` :class:`Blueprint`
+
+.. code-block:: python
+
+   templateview = Blueprint('templateview',import_name=__name__)
+
+* re-used:
+
+  - :meth:`hello.href`
+  - :meth:`basics.getalltemplates`
+
+* define two :py:obj:`view_func`'s:
+
+.. literalinclude:: ../templateview.py
+   :pyobject: view
+
+.. literalinclude:: ../templateview.py
+   :pyobject: listall
+
+
+show cases
+----------
+
+>>> testapp = Flask(__name__)
+
+* raw html outputs of the templates with no context supplied:
+
+>>> with testapp.app_context():
+...     base_htm = render_template('base.htm')
+...     templateview_htm = render_template('templateview.htm')
+...     tableview_htm = render_template('tableview.htm')
+...
+
+>>> base_htm == '''<!DOCTYPE html>
+... <html lang="en">
+... <head>
+...   <meta charset="utf-8">
+... </head>
+... <body>
+... </body>
+... </html>'''
+True
+
+>>> templateview_htm == '''<!DOCTYPE html>
+... <html lang="en">
+... <head>
+...   <meta charset="utf-8">
+...   <link rel="stylesheet" href="/static/css/basic.css"></link>
+...   <link rel="stylesheet" href="/static/css/source.css"></link>
+... </head>
+... <body>
+... <figure>
+... <figurecaption></figurecaption>
+... </figure>
+... </body>
+... </html>'''
+True
+
+>>> tableview_htm == '''<!DOCTYPE html>
+... <html lang="en">
+... <head>
+...   <meta charset="utf-8">
+...   <link rel="stylesheet" href="/static/css/basic.css"></link>
+... </head>
+... <body>
+... <table>
+...   <caption></caption>
+... </table>
+... </body>
+... </html>'''
+True
+
+* ``templateview.htm`` usage: (*note*: ``{{source|safe}}``)
+
+>>> with testapp.app_context():
+...     templateview_htm_1 = render_template(
+...         'templateview.htm',source='<script>alert("hi")</script>')
+>>> templateview_htm_1 == '''<!DOCTYPE html>
+... <html lang="en">
+... <head>
+...   <meta charset="utf-8">
+...   <link rel="stylesheet" href="/static/css/basic.css"></link>
+...   <link rel="stylesheet" href="/static/css/source.css"></link>
+... </head>
+... <body>
+... <figure>
+... <figurecaption></figurecaption>
+... <script>alert("hi")</script>
+... </figure>
+... </body>
+... </html>'''
+True
+
+* ``tableview.htm`` usage:
+
+>>> cells = [
+...     ('cell-00','cell-01','cell-02'),
+...     ('cell-01','cell-11','cell-12'),
+...     ('cell-02','cell-21','cell-22'),
+... ]
+>>> headings = ('column-0','column-1','column-2')
+>>>
+>>> with testapp.app_context():
+...     tableview_htm_1 = render_template(
+...         'tableview.htm',caption='a table',
+...         tables=[dict(records=cells,headings=headings),])
+...
+>>> tableview_htm_1 == '''<!DOCTYPE html>
+... <html lang="en">
+... <head>
+...   <meta charset="utf-8">
+...   <link rel="stylesheet" href="/static/css/basic.css"></link>
+... </head>
+... <body>
+... <table>
+...   <caption>a table</caption>
+...   <thead>
+...     <tr>
+...       <th>column-0</th>
+...       <th>column-1</th>
+...       <th>column-2</th>
+...     </tr>
+...   </thead>
+...   <tbody>
+...     <tr>
+...       <td>cell-00</td>
+...       <td>cell-01</td>
+...       <td>cell-02</td>
+...     </tr>
+...     <tr>
+...       <td>cell-01</td>
+...       <td>cell-11</td>
+...       <td>cell-12</td>
+...     </tr>
+...     <tr>
+...       <td>cell-02</td>
+...       <td>cell-21</td>
+...       <td>cell-22</td>
+...     </tr>
+...   </tbody>
+... </table>
+... </body>
+... </html>'''
+True
+
+"""
+
+from __future__ import print_function, unicode_literals
+import sys, os
+from pygments import highlight
+from pygments.lexers import HtmlLexer
+from pygments.formatters import HtmlFormatter
+from flask import (
+    Flask, Blueprint, render_template,
+    current_app, request, url_for,
+    Markup)
+from basics import getalltemplates
+from hello import href
+
+lexer = HtmlLexer()
+formatter = HtmlFormatter(cssclass='source')
+templateview = Blueprint('templateview',__name__)
+
+@templateview.route('/templatestable')
+def listall():
+    hrefs = [
+        (href(url_for('.view',template=t)),)
+        for t in getalltemplates() ]
+    return render_template(
+        'tableview.htm',caption='templates',
+        tables=[dict(records=hrefs),])
+
+@templateview.route('/view/<template>')
+def view(template='base.htm'):
+    args = request.args.to_dict()
+    html = render_template(template,**args)
+    # show = '<caption>{}</caption>\n'.format(template)
+    show = highlight(html,lexer,formatter)
+    return render_template(
+        'templateview.htm',source=show.strip(),template=template)
+
+
+if __name__ == '__main__':
+
+    import sys
+    import doctest
+
+    app = Flask(__name__)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.register_blueprint(templateview)
+    app.add_url_rule("/",endpoint='templateview.listall')
+
+    if sys.argv[0] != "":
+        app.run(debug=True,use_reloader=True)
+    else:
+        print(doctest.testmod(optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+        exec(doctest.script_from_examples(__doc__))
